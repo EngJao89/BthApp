acfa76cbbecaa49ef03c610a5e136e9ee4de9c1b
EngJao89 <eng_joao@outlook.com>
2025-01-16 23:09:14 -0300

    feat(mbth08-card): implements component of cards incidents

--- app/(dashboard)/userlist/page.tsx
+++ app/(dashboard)/userlist/page.tsx
@@ -1,7 +1,8 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 
-import { Colors } from "@/constants/Colors";
 import logo from '../../../assets/images/logo.png';
+import { Colors } from "@/constants/Colors";
+import { Card } from "@/component/Card";
 
 export default function UserList() {
   return(
@@ -15,6 +16,14 @@ export default function UserList() {
         <Text style={styles.subtitle}>
           Escolha um dos casos abaixo{"\n"}e salve o dia.
         </Text>
+
+        <ScrollView>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </ScrollView>
       </View>
     </SafeAreaView>
   )
@@ -28,7 +37,7 @@ const styles = StyleSheet.create({
   header: {
     padding: 24,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   logo: {
     margin: 2,
@@ -40,15 +49,15 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   title: {
+    color: Colors.zinc_950,
     fontSize: 24,
     fontWeight: 'bold',
-    color: Colors.zinc_950,
     marginTop: 16,
     marginLeft: 16,
   },
   subtitle: {
+    color: Colors.zinc_500,
     fontSize: 16,
-    color: Colors.zinc_400,
     marginTop: 10,
     marginLeft: 16,
     marginBottom: 16,
